770735d10e363fb5b735522932cf57cd67535119
RichardMathieu <mat.richard4@gmail.com>
2018-01-23 15:13:29 +0100

    etape 7

--- project/src/app/exo03/detail-partial/detail.controller.js
+++ project/src/app/exo03/detail-partial/detail.controller.js
@@ -3,15 +3,9 @@
 
   function DetailCtrl($location) {
     var vm = this;
-    vm.msg = 'Welcome All!';
-    vm.subMsg = 'A seed Project';
-    vm.goToExo = goToExo;
+   
 
-    function goToExo(exoNumber) {
-      if (exoNumber === 3) {
-        $location.path('/exo03');
-      }
-    }
+    
   }
 
   DetailCtrl.$inject = ['$location'];
