6119e8705d2cc10b5304a2d9ed0924668bcc92f9
RichardMathieu <mat.richard4@gmail.com>
2018-01-24 11:44:53 +0100

    etape 7

--- project/src/components/meteo/meteo.service.js
+++ project/src/components/meteo/meteo.service.js
@@ -5,7 +5,16 @@
       var meteo = {};
       meteo.meteoListe = [];
       meteo.getMeteo = getMeteo;
-       function getMeteo(){}
+      meteo.settingService=settingService;
+       function getMeteo(){
+        return $http.get('http://api.openweathermap.org/data/2.5/forecast?q='+ meteo.settingService.city+'&APPID=4fcb3a91904bd04021870ca8464a60d8&units=metric')
+            .then(function(response){
+                meteo.meteoListe = response;
+            })
+            .catch(function(error){
+                meteo.meteoListe = [];
+            });
+       }
 
       return  meteo;
     }
