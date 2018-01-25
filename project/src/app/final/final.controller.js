b2d1a7329a0754249de73fa9c8a0f3998106fbee
RichardMathieu <mat.richard4@gmail.com>
2018-01-25 14:23:28 +0100

    etape 2

---
+++ project/src/app/final/final.controller.js
@@ -0,0 +1,16 @@
+(function() {
+  'use strict';
+
+  function FinalCtrl($location,) {
+    var vm = this;
+    
+   
+  }
+ 
+
+  FinalCtrl.$inject = ['$location'];
+
+  angular.module('daproject')
+    .controller('FinalCtrl', FinalCtrl);
+
+})();
