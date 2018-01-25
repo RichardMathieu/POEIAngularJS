04dc64041a6537bbd9d4e48459cf28892c446c59
RichardMathieu <mat.richard4@gmail.com>
2018-01-25 14:39:14 +0100

    etape 3

--- project/src/app/final/final.controller.js
+++ project/src/app/final/final.controller.js
@@ -1,14 +1,21 @@
 (function() {
   'use strict';
 
-  function FinalCtrl($location,) {
+  function FinalCtrl($location,searchConfig) {
     var vm = this;
+    vm.boutton='';
+    vm.buvons=buvons;
+    vm.searchConfig=searchConfig;
+    function buvons() {
+    	vm.searchConfig.ingredient = vm.boutton;
+    	console.log(vm.searchConfig.ingredient)
+    }
     
    
   }
  
 
-  FinalCtrl.$inject = ['$location'];
+  FinalCtrl.$inject = ['$location','searchConfig'];
 
   angular.module('daproject')
     .controller('FinalCtrl', FinalCtrl);
