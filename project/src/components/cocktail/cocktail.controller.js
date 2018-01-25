fd79d21c88e03698ac1a6951a8440e93e8789840
RichardMathieu <mat.richard4@gmail.com>
2018-01-25 15:11:24 +0100

    etape 5

---
+++ project/src/components/cocktail/cocktail.controller.js
@@ -0,0 +1,15 @@
+(function() {
+  'use strict';
+
+  function CocktailCtrl($location) {
+    var vm = this;
+    
+  }
+ 
+
+  Exo04Ctrl.$inject = ['$location',];
+
+  angular.module('daproject')
+    .controller('CocktailCtrl', CocktailCtrl);
+
+})();
